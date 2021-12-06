f7d7f10cf251ee8efd543f11b4cf926b7cd1b7c3
Yago Andrade <ayav@ic.ufal.br>
2021-12-06 16:37:32 -0300

    Rota adicionada

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ Route::get('/', function () {
 # Auth
 Auth::routes(['verify' => true]);
 Route::get('/', [ RoomController::class, 'index']);
+Route::get('/dashboard', [RoomController::class, 'dashboard']);
 Route::get('/sala/create/', [ RoomController::class, 'criar']);
 Route::get('/sala/buscar/{id}', [ RoomController::class, 'buscar']);
 Route::get('/sala/validar/{token}', [ RoomController::class, 'validar']);
